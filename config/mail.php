093b55f97b55f8c4490c07b5856c3d618941d23a
Ing Leonardo <lrmaldo@gmail.com>
2020-02-25 01:19:40 -0600

    cambio de mail

--- config/mail.php
+++ config/mail.php
@@ -80,7 +80,7 @@ return [
 	|
 	*/
 
-	'username' => env('MAIL_USERNAME'),
+	'username' => env('leodeveloper93@gmail.com'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
 	|
 	*/
 
-	'password' => env('MAIL_PASSWORD'),
+	'password' => env('leonardito'),
 
 	/*
 	|--------------------------------------------------------------------------
