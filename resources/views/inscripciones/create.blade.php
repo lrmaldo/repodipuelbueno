76128ce25cf2b77484a30f12b49c6e13bb164bf9
Ing Leonardo <lrmaldo@gmail.com>
2020-02-26 14:01:02 -0600

    cambio de create

--- resources/views/inscripciones/create.blade.php
+++ resources/views/inscripciones/create.blade.php
@@ -185,51 +185,7 @@
 <script type="text/javascript">
 
 
-function GetCount(){
-		
-			año=2020;
-			mes=2;
-			dia=26;
-			hora=12;
-			minuto=0;
-			segundo=0;
-			urlsitio = 'http://www.irineomolina.sattlink.com/'; //url del sitio web final			
 
-			dateFuture = new Date(año, mes-1 , dia, hora, minuto, segundo);	
-		dateNow = new Date();                                             
-		amount = dateFuture.getTime() - dateNow.getTime();                
-		delete dateNow;
-
-		if(amount < 0){
-				location.href=urlsitio;
-		}
-		else{
-				days=0;hours=0;mins=0;secs=0;out="";
-
-				amount = Math.floor(amount/1000);
-
-				days=Math.floor(amount/86400);
-				amount=amount%86400;
-
-				hours=Math.floor(amount/3600);
-				amount=amount%3600;
-
-				mins=Math.floor(amount/60);
-				amount=amount%60;
-
-				secs=Math.floor(amount);
-
-				if(days != 0){out += days +" dia"+((days!=1)?"s":"")+", ";}
-				if(days != 0 || hours != 0){out += hours +" hora"+((hours!=1)?"s":"")+", ";}
-				if(days != 0 || hours != 0 || mins != 0){out += mins +" minuto"+((mins!=1)?"s":"")+", ";}
-				out += secs +" segundos";
-				document.getElementById('countbox').innerHTML=out;
-				
-				setTimeout("GetCount()", 1000);
-				return out;
-		}
-}
-	console.log(GetCount());
 
 </script>
 
